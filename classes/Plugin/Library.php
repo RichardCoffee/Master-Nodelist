400cb2e61821cf5a1fa5391e79c6d0ab93c74f7a
Laptop 3 <richard.coffee@gmail.com>
2018-04-05 23:24:31 -0400

    more log testing

--- classes/Plugin/Library.php
+++ classes/Plugin/Library.php
@@ -7,22 +7,26 @@ class WMN_Plugin_Library {
 	use WMN_Trait_Magic;
 
 	public function __construct() {
-		static $called = false;
-		if ( ! $called ) {
-			self::register_call( array( $this, 'logging_get_calling_function_name' ), 'get_calling_function' );
-			self::register_call( array( $this, 'logging_was_called_by' ),             'was_called_by' );
-			if ( WP_DEBUG ) {
-				add_action( 'deprecated_function_run',    array( $this, 'logging_log_deprecated' ), 10, 3 );
-				add_action( 'deprecated_constructor_run', array( $this, 'logging_log_deprecated' ), 10, 3 );
-				add_action( 'deprecated_file_included',   array( $this, 'logging_log_deprecated' ), 10, 4 );
-				add_action( 'deprecated_argument_run',    array( $this, 'logging_log_deprecated' ), 10, 3 );
-				add_action( 'deprecated_hook_run',        array( $this, 'logging_log_deprecated' ), 10, 4 );
-				add_action( 'doing_it_wrong_run',         array( $this, 'logging_log_deprecated' ), 10, 3 );
-			}
-		} else {
-			$this->log( self::$magic__call );
+#		static $called = false;
+#		if ( ! $called ) {
+			$this->initialize();
+#		} else {
+#			$this->log( self::$magic__call );
+#		}
+#		$called = true;
+	}
+
+	protected function initialize() {
+		self::register_call( array( $this, 'logging_get_calling_function_name' ), 'get_calling_function' );
+		self::register_call( array( $this, 'logging_was_called_by' ),             'was_called_by' );
+		if ( WP_DEBUG ) {
+			add_action( 'deprecated_function_run',    array( $this, 'logging_log_deprecated' ), 10, 3 );
+			add_action( 'deprecated_constructor_run', array( $this, 'logging_log_deprecated' ), 10, 3 );
+			add_action( 'deprecated_file_included',   array( $this, 'logging_log_deprecated' ), 10, 4 );
+			add_action( 'deprecated_argument_run',    array( $this, 'logging_log_deprecated' ), 10, 3 );
+			add_action( 'deprecated_hook_run',        array( $this, 'logging_log_deprecated' ), 10, 4 );
+			add_action( 'doing_it_wrong_run',         array( $this, 'logging_log_deprecated' ), 10, 3 );
 		}
-		$called = true;
 	}
 
 	public function magic__call() {
