e4f08ab1dc6d049907cd7df9801cc31c633b63da
Laptop 3 <richard.coffee@gmail.com>
2018-04-10 14:04:29 -0400

    test object

--- js/master-nodelist.js
+++ js/master-nodelist.js
@@ -4,6 +4,7 @@ jQuery( document ).ready( function( $ ) {
 
 	jQuery( '#active_nodelist' ).on( 'change', function( event ) {
 		var text = jQuery( "#active_nodelist option:selected" ).val();
+console.log(jQuery( "#active_nodelist option:selected" ));
 		jQuery( '#master-nodelist' ).html( '<h1>Node selected was '+text+'</h1>' );
 	}
 
