27224e83fc7e4d05e064bb54d89419b6b6e2b1b3
Laptop 3 <richard.coffee@gmail.com>
2018-04-10 14:15:29 -0400

    console log

--- js/master-nodelist.js
+++ js/master-nodelist.js
@@ -4,6 +4,8 @@
 //} );
 
 function load_nodelist() {
+console.log(jQuery( "#active_nodelist" );
+console.log(jQuery( "#active_nodelist option:selected" );
 	var text = jQuery( "#active_nodelist option:selected" ).text();
 	jQuery( '#master-nodelist' ).html( '<h1>Node selected was '+text+'</h1>' );
 //	jQuery( '#master-nodelist' ).html( '<h1>A node was selected</h1>' );
