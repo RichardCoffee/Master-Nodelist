c78a96acc0a9ecf6743c2951234701e22fbd5948
Laptop 3 <richard.coffee@gmail.com>
2018-04-10 10:28:12 -0400

    add missing return value

--- includes/wpfep.php
+++ includes/wpfep.php
@@ -3,7 +3,7 @@
 
 function wmn_wpfep_tech_tab( $tabs = array() ) {
 	$tabs[] = array(
-		'id'            => 'tech',
+		'id'            => 'tech_tab',
 		'label'         => __('Tech Info','wmn-workbook'),
 		'tab_class'     => 'tech-tab',
 		'content_class' => 'tech-content',
@@ -18,6 +18,8 @@ function wmn_wpfep_tech_tab_meta_fields( $fields = array() ) {
 		'label'   => __('Tech ID','wmn-workbook'),
 		'desc'    => __('Your tech id number for spreadsheet.','wmn-workbook'),
 		'type'    => 'text',
-		'classes' => 'tech-id-field');
+		'classes' => 'tech-id-field'
+	);
+	return $fields;
 }
-add_filter('wpfep_fields_tech','wmn_wpfep_tech_tab_meta_fields');
+add_filter('wpfep_fields_tech_tab','wmn_wpfep_tech_tab_meta_fields');
