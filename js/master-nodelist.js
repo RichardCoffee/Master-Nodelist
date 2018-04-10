e65d21321c660e98e9396b05846c470509480158
Laptop 3 <richard.coffee@gmail.com>
2018-04-10 14:18:40 -0400

    fix id

--- js/master-nodelist.js
+++ js/master-nodelist.js
@@ -4,9 +4,9 @@
 //} );
 
 function load_nodelist() {
-console.log(jQuery( "#active_nodelist" ) );
-console.log(jQuery( "#active_nodelist option:selected" ) );
-	var text = jQuery( "#active_nodelist option:selected" ).text();
+console.log(jQuery( "#active_node" ) );
+console.log(jQuery( "#active_node option:selected" ) );
+	var text = jQuery( "#active_node option:selected" ).text();
 	jQuery( '#master-nodelist' ).html( '<h1>Node selected was '+text+'</h1>' );
 //	jQuery( '#master-nodelist' ).html( '<h1>A node was selected</h1>' );
 }
