60b367f1773bb14afbdb564a528df71833fdc0b7
Laptop 3 <richard.coffee@gmail.com>
2018-04-11 21:05:42 -0400

    add scroll call

--- js/master-nodelist.js
+++ js/master-nodelist.js
@@ -3,7 +3,7 @@
 //jQuery( document ).ready( function( $ ) {
 //} );
 
-function load_nodelist(page) {
+function load_nodelist( page, scroll ) {
 	var outdata = {
 		action:  "wmn_show_nodelist",
 		active:   jQuery( "#active_node option:selected" ).val(),
@@ -11,6 +11,9 @@ function load_nodelist(page) {
 		security: nodelist_ajax.security
 	};
 	contact_server( '#master-nodelist', outdata );
+	if ( scroll ) {
+		scrollToElement( '#master-nodelist' );
+	}
 }
 
 function contact_server( contentDiv, outData, wait ) {
@@ -21,19 +24,26 @@ function contact_server( contentDiv, outData, wait ) {
 		type: 'post',
 		data:  outData,
 		async: aSync, // wtf chrome?  aSync, // jQuery default is true
-		success: function(result,textStatus,jqXHR) {
-			if (result) {
-				jQuery(contentDiv).html(result);
+		success: function( result, textStatus, jqXHR ) {
+			if ( result ) {
+				jQuery( contentDiv ).html( result );
 				value = true;
 			}
 		},
-		error: function(jqXHR,textStatus,errorThrown) {
-			alert('Server Error: '+errorThrown+' '+textStatus);
-			console.log('server error: '+errorThrown);
-			console.log(nodelist_ajax.ajaxurl);
-			console.log(outData);
+		error: function( jqXHR, textStatus, errorThrown ) {
+			alert( 'Server Error: ' + errorThrown + ' ' + textStatus );
+			console.log( 'server error: ' + errorThrown );
+			console.log( nodelist_ajax.ajaxurl );
+			console.log( outData );
 		}
 	});
 	return value;
 }
 
+function pick_entry( el, id ) {
+	var outData = {
+		action: 'wmn_pick_entry',
+		active:   jQuery( "#active_node option:selected" ).val(),
+		security: nodelist_ajax.security
+	}
+}
