6fb3f9d2963e2cf0509570ef20468038c53a0833
Laptop 3 <richard.coffee@gmail.com>
2018-04-12 14:30:41 -0400

    testing addon span

--- classes/Form/Field/Text.php
+++ classes/Form/Field/Text.php
@@ -4,7 +4,10 @@ class WMN_Form_Field_Text extends WMN_Form_Field_Field {
 
 
 	public function text() { ?>
-		<div class="input-group"><?php
+		<div class="input-group">
+			<span class="input-group-addon"></span>
+<?php
+#			$this->addon();
 			$this->label();
 			$this->input(); ?>
 		</div><?php
